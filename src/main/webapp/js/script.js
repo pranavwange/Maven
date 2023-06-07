9bb0e46e0a202c1cdb71501e4c651d0825bdc588
pranavwange <pranavwange@gmail.com>
2023-06-07 08:34:32 +0530

    Second Commit

---
+++ src/main/webapp/js/script.js
@@ -0,0 +1,6 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+  var message = "Hello, World!";
+  var heading = document.querySelector("h1");
+  heading.textContent = message;
+});
